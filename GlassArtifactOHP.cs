69d1c061d313b915c5d8e07ed62480434bf42554
ThinkInvisible <ThinkInvis@users.noreply.github.com>
2020-04-19 21:07:56 -0500

    Implement Artifact of Danger

--- GlassArtifactOHP.cs
+++ GlassArtifactOHP.cs
@@ -3,35 +3,75 @@ using BepInEx;
 using MonoMod.Cil;
 using UnityEngine;
 using System;
+using System.Collections.Generic;
+using R2API;
+using Mono.Cecil.Cil;
+using System.Reflection;
+using R2API.Utils;
+using R2API.AssetPlus;
 
 namespace ThinkInvisible.GlassArtifactOHP {
     
     [BepInDependency("com.bepis.r2api")]
     [BepInPlugin(ModGuid, ModName, ModVer)]
+    [R2APISubmoduleDependency(nameof(AssetPlus), nameof(ResourcesAPI))]
     public class GlassArtifactOHPPlugin:BaseUnityPlugin {
         public const string ModVer = "2.0.0";
         public const string ModName = "GlassArtifactOHP";
         public const string ModGuid = "com.ThinkInvisible.GlassArtifactOHP";
         
+        public ArtifactDef dangerArtifact {get;private set;}
+
+        private bool dangerEnabled;
+
         public void Awake() {
+            using(var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GlassArtifactOHP.glassohp_assets")) {
+                var bundle = AssetBundle.LoadFromStream(stream);
+                var provider = new AssetBundleResourcesProvider("@GlassArtifactOHP", bundle);
+                ResourcesAPI.AddProvider(provider);
+            }
+
             IL.RoR2.CharacterBody.RecalculateStats += IL_CBRecalcStats;
+            ArtifactCatalog.getAdditionalEntries += Evt_ACGetAdditionalEntries;
+            On.RoR2.RunArtifactManager.SetArtifactEnabled += RunArtifactManager_SetArtifactEnabled;
+        }
+
+        private void RunArtifactManager_SetArtifactEnabled(On.RoR2.RunArtifactManager.orig_SetArtifactEnabled orig, RunArtifactManager self, ArtifactDef artifactDef, bool newEnabled) {
+            if(artifactDef == dangerArtifact) {
+                dangerEnabled = newEnabled;
+            }
+            orig(self, artifactDef, newEnabled);
+        }
+
+        private void Evt_ACGetAdditionalEntries(List<ArtifactDef> addEnts) {
+            dangerArtifact = ScriptableObject.CreateInstance<ArtifactDef>();
+            dangerArtifact.nameToken = "Danger";
+            dangerArtifact.descriptionToken = "Allows enemies to kill you with one hit.";
+            dangerArtifact.smallIconDeselectedSprite = Resources.Load<Sprite>("@GlassArtifactOHP:Assets/GlassArtifactOHP/danger-off.png");
+            dangerArtifact.smallIconSelectedSprite = Resources.Load<Sprite>("@GlassArtifactOHP:Assets/GlassArtifactOHP/danger-on.png");
+
+            //smallIconSelectedSprite, smallIconDeselectedSprite
+            addEnts.Add(dangerArtifact);
         }
 
         private void IL_CBRecalcStats(ILContext il) {
             ILCursor c = new ILCursor(il);
-            bool ILFound = c.TryGotoNext(MoveType.After,
-                x => x.MatchLdcI4(0),
-                x => x.MatchStloc(36),
-                x => x.MatchLdarg(0),
-                x => x.MatchLdarg(0),
-                x => x.MatchCallvirt<CharacterBody>("get_isPlayerControlled")
+            bool ILFound = c.TryGotoNext(
+                x=>x.OpCode == OpCodes.Ldloc_S,
+                x=>x.MatchLdcI4(0),
+                x=>x.MatchCeq(),
+                x=>x.OpCode == OpCodes.Br_S,
+                x=>x.MatchLdcI4(0),
+                x=>x.MatchCallOrCallvirt<CharacterBody>("set_hasOneShotProtection")
                 );
             if(ILFound) {
-                c.RemoveRange(6);
+                c.RemoveRange(3);
+                c.EmitDelegate<Func<bool>>(()=>{
+                    return !RunArtifactManager.instance.IsArtifactEnabled(dangerArtifact);
+                });
             } else {
                 Debug.LogError("GlassArtifactOHP: failed to apply IL patch! Mod not loaded.");
             }
         }
-    }
     }
 }
